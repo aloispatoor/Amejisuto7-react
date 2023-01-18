e9f9df4eb50299db9cfb16a1d1543e5c73e30a2f
Alois Patoor <palois89@posteo.net>
2023-01-18 10:51:22 +0100

    remove react-typed dependency

--- src/components/OpenOrClosed.jsx
+++ src/components/OpenOrClosed.jsx
@@ -1,16 +1,53 @@
 import '../style/AnimCommission.css';
-import Typed from 'react-typed';
+import React from "react";
+import  { motion } from "framer-motion";
+
+const AnimatedText = ({ text }) => {
+    const words = text.split("");
+
+    const container = {
+        hidden: { opacity: 0 },
+        visible: (i = 1) => ({
+            opacity: 1,
+            transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+        }),
+    };
+
+    const child = {
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                type: "spring",
+                damping: 12,
+                stiffness: 100,
+            },
+        },
+        hidden: {
+            opacity: 0,
+            x: 20,
+            transition: {
+                type: "spring",
+                damping: 12,
+                stiffness: 100,
+            },
+        },
+    };
+
+    console.log(words);
+    return (
+        <motion.div variants={container}>{words.map((word, index) => (
+            <motion.span key={index} variants={child}>{word}</motion.span>
+            ))}
+        </motion.div>
+    );
+};
 
 function OpenOrClosed(){
     
     return(
         <section id="anim-commissions">
-            <Typed
-                strings={[
-                    "Commissions are OPEN",
-                ]}
-                typeSpeed={150}
-            />
+            <AnimatedText text="Commissions are CLOSED"/>
         </section>
     )
 }
